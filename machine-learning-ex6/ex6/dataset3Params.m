8c43e44b7ac5c936376ac3a7b281ca7a6f8eb90b
Vimal Kumar <vimaku@amazon.com>
2021-02-13 16:54:13 +0530

    Sixth assignment solutions

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,9 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+CVal = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigmaVal = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+error = zeros(length(CVal), length(sigmaVal));
 
+for i = 1:length(CVal)
+	for j = 1:length(sigmaVal)
+		model = svmTrain(X, y, CVal(i), @(x1, x2) gaussianKernel(x1, x2, sigmaVal(j)));
+		predictions = svmPredict(model, Xval);
+		error(i,j) = mean(double(predictions ~= yval));
+	end
+end
 
+[val, row] = min(min(error, [], 2));
+[val, col] = min(min(error));
+
+C = CVal(row);
+sigma = sigmaVal(col);
 
 
 
